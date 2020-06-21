eb150af7b419942ebe7f4ffbeec7c07d7b8cdc37
namithkumar <65290789+namithkumar@users.noreply.github.com>
2020-06-21 18:43:25 +0530

    Add files via upload

--- Divisions.js
+++ Divisions.js
@@ -1,18 +1,18 @@
 class Divisions {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
-    }
-    display(){
+    constructor(x, y, w, h) {
+        var options = {
 
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
     }
-  };
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
